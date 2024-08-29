8bc135110442e3eeeb923caa96411254e72f633e
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-08-29 10:07:41 -0500

    Update syn-config.sh

--- shell-scripts/syn-config.sh
+++ shell-scripts/syn-config.sh
@@ -4,6 +4,6 @@ sudo openssl genrsa -out "/opt/keys/syn_private.key" 2048
 sudo openssl rsa -pubout -in "/opt/keys/syn_private.key" -out "/opt/keys/syn_public.key"
 sudo chown www-data:www-data /opt/keys/syn*
 sudo mkdir /opt/syn
-sudo cp /mnt/hgfs/shared/configs/fedora_configs/tomcat-and-syn-for-fedora/syn-settings.xml /mnt/fcrepo/config/
+sudo cp /mnt/shared/configs/fedora_configs/tomcat-and-syn-for-fedora/syn-settings.xml /mnt/fcrepo/config/
 sudo chown tomcat:tomcat /mnt/fcrepo/config/syn-settings.xml
 sudo chmod 640 /mnt/fcrepo/config/syn-settings.xml
