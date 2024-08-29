74ac3c2cc3e3ff2da66f8e03f61d7fb9c7684f58
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-08-29 10:07:11 -0500

    Update scratch_5.sh

--- shell-scripts/scratch_5.sh
+++ shell-scripts/scratch_5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #scratch_5 / tomcat configuration
 #Make sure version of openjdk java is correct in JAVA_HOME:
-sudo cp /mnt/hgfs/shared/configs/tomcat_conf/setenv.sh /opt/tomcat/bin/
+sudo cp /mnt/shared/configs/tomcat_conf/setenv.sh /opt/tomcat/bin/
 sudo chmod 755 /opt/tomcat/bin/setenv.sh
-sudo cp /mnt/hgfs/shared/configs/tomcat_conf/tomcat.service /etc/systemd/system/
+sudo cp /mnt/shared/configs/tomcat_conf/tomcat.service /etc/systemd/system/
 sudo chmod 755 /etc/systemd/system/tomcat.service
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
