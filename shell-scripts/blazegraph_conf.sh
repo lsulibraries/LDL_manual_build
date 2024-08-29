23d2153fb6b3c2e086bce646e1a18f431ba8e0cb
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-08-29 10:01:54 -0500

    Update blazegraph_conf.sh

--- shell-scripts/blazegraph_conf.sh
+++ shell-scripts/blazegraph_conf.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #Configuring Logging
-sudo cp /mnt/hgfs/shared/configs/blazegraph/log4j.properties /opt/blazegraph/conf/
+sudo cp /mnt/shared/configs/blazegraph/log4j.properties /opt/blazegraph/conf/
 sudo chown tomcat:tomcat /opt/blazegraph/conf/log4j.properties
 sudo chmod 644 /opt/blazegraph/conf/log4j.properties
 #Adding a Blazegraph Configuration
-sudo cp /mnt/hgfs/shared/configs/blazegraph/RWStore.properties /opt/blazegraph/conf
+sudo cp /mnt/shared/configs/blazegraph/RWStore.properties /opt/blazegraph/conf
 sudo chown tomcat:tomcat /opt/blazegraph/conf/RWStore.properties
 sudo chmod 644 /opt/blazegraph/conf/RWStore.properties
-sudo cp /mnt/hgfs/shared/configs/blazegraph/blazegraph.properties /opt/blazegraph/conf
+sudo cp /mnt/shared/configs/blazegraph/blazegraph.properties /opt/blazegraph/conf
 sudo chown tomcat:tomcat /opt/blazegraph/conf/blazegraph.properties
 sudo chmod 644 /opt/blazegraph/conf/blazegraph.properties
-sudo cp /mnt/hgfs/shared/configs/blazegraph/inference.nt /opt/blazegraph/conf
+sudo cp /mnt/shared/configs/blazegraph/inference.nt /opt/blazegraph/conf
 sudo chown tomcat:tomcat /opt/blazegraph/conf/inference.nt
 sudo chmod 644 /opt/blazegraph/conf/inference.nt
 
