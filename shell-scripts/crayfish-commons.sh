695fde676fcc9a527f6c079aa1abb46b3ef67ec8
Milad Khanlou <104443908+miladkhanlou@users.noreply.github.com>
2024-08-30 10:18:05 -0500

    Update crayfish-commons.sh

--- shell-scripts/crayfish-commons.sh
+++ shell-scripts/crayfish-commons.sh
@@ -3,4 +3,4 @@ sudo mkdir /opt/crayfish/Commons
 cd /opt/crayfish/Commons
 sudo chown -R www-data:www-data /opt/crayfish/Commons
 sudo chmod -R 755 /opt/crayfish/Commons
-composer require islandora/crayfish-commons
+sudo -u www-data:www-data composer require islandora/crayfish-commons
